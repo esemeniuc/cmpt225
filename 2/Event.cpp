4cbb7048c1b3ab200b0c523f53a4470af4198f40
cwaffles <cwaffles@users.noreply.github.com>
2016-06-18 16:04:08 -0700

    base Event.cpp added

--- 2/Event.cpp
+++ 2/Event.cpp
@@ -9,3 +9,100 @@
 //on the computer monitor screen. Name this class Event (Event.h and Event.cpp).
 
 #include "Event.h"
+
+//Class Invariant: Type must be 'A' or 'D', time and length attributes cannot be negative
+
+/*private attributes/methods
+
+	char type; //Event type, either A or D
+	unsigned int time; //Time of customer arrival
+	unsigned int length; //Length of time it takes to process the event
+*/
+
+//public attributes/methods
+
+//Description: default constructor
+//Preconditions: None
+//Postconditions: constructs an empty Event with type = 'A' (Arrival), time = 0, and length = 0
+Event::Event(void)
+{
+
+}
+
+//Description: Constructor with time and length inputs
+//Preconditions: inputTime and inputLength are non negative
+//Postconditions: constructs an empty Event with type = 'A' (Arrival), time = inputTime, and length = inputLength
+Event::Event(unsigned int inputTime, unsigned int inputLength)
+{
+
+}
+
+//Description: Constructor with type, time, and length inputs
+//Preconditions: inputType is 'A' or 'D', inputTime and inputLength are non negative
+//Postconditions: constructs an empty Event with type = inputType, time = inputTime, and length = inputLength
+Event::Event(char inputType, unsigned int inputTime, unsigned int inputLength)
+{
+
+}
+
+//Description: default destructor
+//Preconditions: the object exists
+//Postconditions: the object no longer exists
+Event::~Event()
+{
+
+} //default destructor
+
+//Getters
+
+//Description: returns the type of the event
+//Preconditions: none
+//Postconditions: returns -1 if the type is not valid, 1 if the event is an arrival, 2 if the event is a departure
+int Event::getType()
+{
+
+}
+
+//Description: returns the arrival time of the event
+//Preconditions: time is non negative
+//Postconditions: returns the arrival time of the event in minutes
+int Event::getTime()
+{
+
+}
+
+//Description: returns the processing time of the event
+//Preconditions: length is non negative
+//Postconditions: returns the processing time of the event in minutes
+int Event::getLength()
+{
+
+}
+
+//Setters
+
+//Description: sets the type of the event
+//Preconditions: Type is either 'A' or 'D'
+//Postconditions: returns 1 if the type is not valid, 0 if it was successfully set
+int Event::setType(char inputType)
+{
+
+}
+
+//Description: sets the arrival time of the event
+//Preconditions: inputTime is a non negative integer
+//Postconditions: returns 1 if the time is not valid, 0 if it was successfully set
+int Event::setTime(unsigned int inputTime)
+{
+
+}
+
+//Description: sets the processing length of the event
+//Preconditions: inputLength is a non negative integer
+//Postconditions: returns 1 if the length is not valid, 0 if it was successfully set
+int Event::setLength(unsigned int inputLength)
+{
+
+}
+
+
